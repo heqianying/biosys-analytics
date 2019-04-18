1adc87ffcb3bd2f8df9470855df1553c0e9f3e96
Qianying He <qhe0310@login2.cm.cluster>
2019-04-18 08:23:54 -0700

    all done

---
+++ assignments/12-unclustered-proteins/find_unclustered.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+"""
+Author : qianying
+Date   : 2019-04-08
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import sys
+import Bio
+import os
+import re
+from Bio import SeqIO
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Find unclustered proteins',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+
+    parser.add_argument(
+        '-c',
+        '--cdhit',
+        help='Output file from CD-HIT (clustered proteins)',
+        metavar='str',
+        type=str,
+        required=True,
+        default='')
+
+    parser.add_argument(
+        '-p',
+        '--proteins',
+        help='Proteins FASTA',
+        metavar='str',
+        type=str,
+        required=True,
+        default='')
+
+    parser.add_argument(
+        '-o',
+        '--outfile',
+        help='Output file',
+        metavar='str',
+        type=str,
+        default='unclustered.fa')
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    cdhit_file = args.cdhit
+    protein_file = args.proteins
+    outfile = args.outfile
+
+    #print('boo')
+
+    # if cdhit_file == '' or protein_file == '':
+    #     die('usage: find_unclustered.py [-h] -c str -p str [-o str]\nfind_unclustered.py: error: the following arguments are required: -c/--cdhit, -p/--proteins')
+
+    if not os.path.isfile(protein_file):
+        print('--proteins "{}" is not a file'.format(protein_file), file=sys.stderr)
+        sys.exit(1)
+
+    if not os.path.isfile(cdhit_file):
+        print('--cdhit "{}" is not a file'.format(cdhit_file), file=sys.stderr)
+        sys.exit(1)
+
+
+    id_re = re.compile('>gi'
+                        '[|]'
+                        '(?P<id_string>\d+)'   # capture year (group 1)
+                        '[|]')
+
+    clust_prots = []
+
+    lines = 0
+
+    with open(cdhit_file, 'r') as f:
+        for line in f:
+            # Do something with 'line'
+            #print(line)
+            #print(re.search('>gi|.*|', line))
+
+            #lines += 1
+
+            match = id_re.search(line)
+            if match:
+                clust_prots.append(match.group('id_string'))
+            # else:
+            #     print(line)
+            # t = re.search(">gi|(\d{9})",line)
+            # print(t)
+
+    #print('lines: {}'.format(lines))
+
+    #print(len(clust_prots))
+    clust_prots = set(clust_prots)
+
+    #print(len(clust_prots))
+
+    out_fh = open(outfile, 'wt')
+
+    num_unclustered = 0
+    num_clustered = 0
+    num_total = 0
+
+    with open(protein_file, "r") as handle:
+        for record in SeqIO.parse(handle, "fasta"):
+            #print(record.id)
+            num_total += 1
+            record.id = re.sub('\|.*', '', record.id)
+            #print(record.id)
+
+            if record.id in clust_prots:
+                #print(record.id)
+                num_clustered += 1
+            else:
+                num_unclustered += 1
+                SeqIO.write(record, out_fh, 'fasta')
+
+    print('Wrote {:,} of {:,} unclustered proteins to "{}"'.format(num_unclustered,num_total,outfile))
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
